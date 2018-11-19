d97e234e74452396b1991466fcdbedbfec0094b2
catherineliu <catherineliu@orangeblock.tech>
2018-11-19 15:20:43 +0800

    打印登录结果

--- Android/FaceBookDemo/app/src/main/java/bcaasc/io/facebookdemo/MainActivity.java
+++ Android/FaceBookDemo/app/src/main/java/bcaasc/io/facebookdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -15,6 +16,8 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.w3c.dom.Text;
+
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
     private Button button;
+    private TextView tvContent;
     private CallbackManager callbackManager;
 
     @Override
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         button = (Button) findViewById(R.id.btn_login);
+        tvContent = (TextView) findViewById(R.id.tv_result);
         loginButton.setReadPermissions("email");
 
         callbackManager = CallbackManager.Factory.create();
@@ -67,17 +72,26 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 // App code
                 System.out.println("onSuccess:" + loginResult);
+                // 您可以通过 SDK 从缓存或应用书签中（如果应用冷启动）加载 AccessToken.getCurrentAccessToken。您应在 Activity 的 onCreate 方法中检查它的有效性：
+                AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                tvContent.setText("ApplicationId:" + accessToken.getApplicationId() +
+                        ";\nUserId:" + accessToken.getUserId() +
+                        ";\ngetDataAccessExpirationTime:" + accessToken.getDataAccessExpirationTime() +
+                        ";\ngetDeclinedPermissions:" + accessToken.getDeclinedPermissions() +
+                        ";\ngetExpires:" + accessToken.getExpires() +
+                        ";\ngetPermissions:" + accessToken.getPermissions() +
+                        ";\ngetSource:" + accessToken.getSource());
             }
 
             @Override
             public void onCancel() {
                 // App code
-                System.out.println("onCancel");
+                tvContent.setText("onCancel");
             }
 
             @Override
             public void onError(FacebookException error) {
-                System.out.println("onError:" + error);
+                tvContent.setText("onError:" + error);
 
             }
         });
