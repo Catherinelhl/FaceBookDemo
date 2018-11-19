bfa09835513e736e741d4a800b400c5f0c6c9e61
catherineliu <catherineliu@orangeblock.tech>
2018-11-19 16:19:31 +0800

    显示用户的profile以及token信息

--- Android/FaceBookDemo/app/src/main/java/bcaasc/io/facebookdemo/MainActivity.java
+++ Android/FaceBookDemo/app/src/main/java/bcaasc/io/facebookdemo/MainActivity.java
@@ -1,9 +1,9 @@
 package bcaasc.io.facebookdemo;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,11 +12,11 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView tvContent;
     private CallbackManager callbackManager;
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,16 +72,58 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                System.out.println("onSuccess:" + loginResult);
-                // 您可以通过 SDK 从缓存或应用书签中（如果应用冷启动）加载 AccessToken.getCurrentAccessToken。您应在 Activity 的 onCreate 方法中检查它的有效性：
-                AccessToken accessToken = AccessToken.getCurrentAccessToken();
-                tvContent.setText("ApplicationId:" + accessToken.getApplicationId() +
-                        ";\nUserId:" + accessToken.getUserId() +
-                        ";\ngetDataAccessExpirationTime:" + accessToken.getDataAccessExpirationTime() +
-                        ";\ngetDeclinedPermissions:" + accessToken.getDeclinedPermissions() +
-                        ";\ngetExpires:" + accessToken.getExpires() +
-                        ";\ngetPermissions:" + accessToken.getPermissions() +
-                        ";\ngetSource:" + accessToken.getSource());
+                System.out.println("onSuccess:" + loginResult.getAccessToken());
+                final AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = null;
+                if (Profile.getCurrentProfile() == null) {
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                            // profile2 is the new profile
+                            System.out.println("facebook - profile:" + profile2.getFirstName());
+                            profile = profile2;
+//                            ";\ngetDataAccessExpirationTime:\t" + accessToken.getDataAccessExpirationTime() +
+//                                    ";\ngetDeclinedPermissions:\t" + accessToken.getDeclinedPermissions() +
+                            tvContent.setText("ApplicationId:\t" + accessToken.getApplicationId() +
+                                    ";\nUserId:\t" + accessToken.getUserId() +
+                                    ";\nToken:\t" + accessToken.getToken() +
+                                    ";\ngetExpires:\t" + accessToken.getExpires() +
+                                    ";\ngetPermissions:\t" + accessToken.getPermissions() +
+                                    ";\ngetSource:\t" + accessToken.getSource() +
+                                    ";\ngetFirstName:\t" + profile.getFirstName() +
+                                    ";\ngetMiddleName:\t" + profile.getMiddleName() +
+                                    ";\ngetLastName:\t" + profile.getLastName() +
+                                    ";\ngetName:\t" + profile.getName() +
+                                    ";\ngetLinkUri:\t" + profile.getLinkUri() +
+                                    ";\ngetId:\t" + profile.getId());
+                            if (mProfileTracker != null) {
+                                mProfileTracker.stopTracking();
+                            }
+
+                        }
+                    };
+                    // no need to call startTracking() on mProfileTracker
+                    // because it is called by its constructor, internally.
+                } else {
+                    profile = Profile.getCurrentProfile();
+                    System.out.println("facebook - profile:" + profile.getFirstName());
+//                    ";\ngetDataAccessExpirationTime:\t" + accessToken.getDataAccessExpirationTime() +
+//                            ";\ngetDeclinedPermissions:\t" + accessToken.getDeclinedPermissions() +
+                    tvContent.setText("ApplicationId:\t" + accessToken.getApplicationId() +
+                            ";\nUserId:\t" + accessToken.getUserId() +
+                            ";\nToken:\t" + accessToken.getToken() +
+                            ";\ngetExpires:\t" + accessToken.getExpires() +
+                            ";\ngetPermissions:\t" + accessToken.getPermissions() +
+                            ";\ngetSource:\t" + accessToken.getSource() +
+                            ";\ngetFirstName:\t" + profile.getFirstName() +
+                            ";\ngetMiddleName:\t" + profile.getMiddleName() +
+                            ";\ngetLastName:\t" + profile.getLastName() +
+                            ";\ngetName:\t" + profile.getName() +
+                            ";\ngetLinkUri:\t" + profile.getLinkUri() +
+                            ";\ngetId:\t" + profile.getId());
+                }
+
+
             }
 
             @Override
